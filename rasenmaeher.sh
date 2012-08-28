a84c6a3e4798bff629f31486a31d115011083aec
seeloeweseeleweh <seeloewe.seele.weh@piratenpartei-nrw.de>
2012-08-29 01:57:36 +0300

    update rasenmaeher. macht jetzt dailies

--- rasenmaeher.sh
+++ rasenmaeher.sh
@@ -6,59 +6,15 @@
 
 bSomeDateToBeMoved="$(date --date='today -1 days' +%Y%m%d)"
 bFolderDaily="dailyMow"
+bFolderWacholder="dailies"
 
-if [ -f "$bSomeDateToBeMoved"* ]
-then
+if ls "$bFolderWacholder"/"$bSomeDateToBeMoved"* > /dev/null 2>&1 
+then 
    bFoldername="$bFolderDaily""$bSomeDateToBeMoved"
    mkdir "$bFoldername"
-   for bFileFromLastDay in $bSomeDateToBeMoved*
+   for bFileFromLastDay in ls "$bFolderWacholder"/"$bSomeDateToBeMoved"*
    do
-      echo 'Wacholder '"$bFileFromLastDay"' nach '"$bFoldername"' verschoben.'
       mv "$bFileFromLastDay" "$bFoldername"
-      echo 'Fertig gemäht!'
    done
-else
-   echo "Keine Wacholder von gestern gefunden."
+   echo 'Fertig gemäht!'
 fi
-
-# Weely mowing happens here
-#
-# get week old juniper folder and move it to the weekly junipers
-
-#bSomeDateToBeMoved="$(date --date='today -1 weeks -1days' +%Y%m%d)"
-#bFolderDailyWeekly="$bFolderDaily""$bSomeDateToBeMoved"
-#bFolderWeekly="weeklyMows"
-
-#if [ ! -d "$bFolderWeekly" ] # Check for weekly folder
-#then
-#   mkdir "$bFolderWeekly"
-#fi
-
-#if [ -d "$bFolderDailyWeekly"* ] # Check for one week old juniper
-#then
-#   mv "$bFolderDailyWeekly" "$bFolderWeekly"
-#fi
-
-# Monthly mowing happens here
-#
-# check if the folder for weekly junipers is full, then 
-# move one into the monthly folder
-
-#bFolderMonthly="monthlyMows"
-
-#if [ ! -d "$bFolderMonthly" ] # Check for monthly folder
-#then
-#   mkdir "$bFolderMonthly"
-#fi
-
-#bNumberOfFolders=$( find "$bFolderMonthly" -maxdepth 1 -type d | wc -l)
-
-#if [[ $bNumberOfFolders -ge 7 ]]
-#then
-#   for folder in "$bFolderMonthly"/
-#   do 
-#      echo $folder
-#      echo $(date --d="$(stat -c %y $folder)" +%Y%m%d)
-#   done
-#fi
-
