9ce2c5188110181aa1f2d3ff70567bf19751e992
seeloewe.seele.weh <seeloewe.seele.weh@piratenpartei.de>
2012-09-03 10:07:01 +0200

    wacholder encoding und unifying

--- wacholder.sh
+++ wacholder.sh
@@ -35,11 +35,20 @@
 #
 ###############################################################################
 
+###
+#
+# variables needed
+#
+
+wConfigFile="$1"
+
+encoded=''
+unified=''
+
 # cd to script's directory
 wPathWorkingDirectory=$( cd "$( dirname "$0" )" && pwd )
 cd "$wPathWorkingDirectory"
 
-wConfigFile='/home/nanooq/.wacholder/execution/wacholder.cfg'
 
 ###
 # 
@@ -56,11 +65,42 @@ source "$wConfigFile"
 # Functions
 #
 
-encoding() { # $1 string to check for encoding
-   if [[ "$1" == *=?* ]]
+# Returns unified
+unifying() { # $1 string to unify
+   unified="$1"
+   unified=$(echo "$unified" | sed 's/Re: //' )	# Re:
+   if [[ unified == *=?* ]]
    then
-      echo "$1" >> "$wFileEnc"
+      decoding "$unified"
    fi
+# TODO take care of Tags, "Re:" and "Forwards"
+# TODO remove not meant separators from text before adding them
+# TODO cut filename at some point. 255 seems to be maximum, but 100 should be enough.
+}
+
+# https://www.ietf.org/rfc/rfc2047.txt
+# Returns encoded
+decoding() { # $1 string to check for encoded
+   if [[ "$encoded" == *=?utf-8?b?* ]] # UTF-8 base 64 unifyTitle
+   then 
+      continue;
+   elif [[ "$encoded" == *=?utf-8?q?* ]] # UTF-8 Q-unifyTitle
+   then 
+      encoded=$(echo "$encoded" | sed 's/=?utf-8?q?//' )	# coding
+      encoded=$(echo "$encoded" | sed 's/=C3=BC/ü/g' )		#ü
+      encoded=$(echo "$encoded" | sed 's/_/ /g' )		#_
+      encoded=$(echo "$encoded" | sed 's/=C3=96/Ö/g' )		#Ö
+      encoded=$(echo "$encoded" | sed 's/=C3=A4/ä/g' )		#ä
+      encoded=$(echo "$encoded" | sed 's/=22/"/g' )		#"
+      encoded=$(echo "$encoded" | sed 's/=C3=B6/ö/g' )		#"
+      encoded=$(echo "$encoded" | sed 's/=C3=9F/ß/g' )		#ß
+      encoded=$(echo "$encoded" | sed 's/=2E/./g' )		#.
+      encoded=$(echo "$encoded" | sed 's/?=//' )		#?=
+   fi
+}
+
+usage() {
+   echo "Usage: cat email | wacholder.sh path/to/config.file"
 }
 
 ###
@@ -69,30 +109,39 @@ encoding() { # $1 string to check for encoding
 #
 
 wPipedEmail="$(cat)" # Store piped e-mail text
-# Generate a re   presentative filename
+
+###
+#
+# Skript
+#
+
+# Prepare creation of a representative filename
 # Date: Extract e-mail's date for 
 wDateString=$(echo "$wPipedEmail" | sed '/^Date: */!d; s///; q' | sed 's/.*< *//;s/ *>.*//;') 
 wDate=$(date -d "$wTupelDate" +%Y%m%d)
 # Mailinglist: Extract mailinglist
 wMailinglist=$(echo "$wPipedEmail" | sed '/^List-Id: */!d; s///; q' | sed 's/.*< *//;s/ *>.*//;')
-encoding "$wMailinglist" # TODO take care of encoding
+unifying "$wMailinglist" # Unify mailinglist name
+wMailinglist="$unified"
 # Subject: Extract subject
 wSubject=$(echo "$wPipedEmail" | sed '/^Subject: */!d; s///; q' | sed 's/.*< *//;s/ *>.*//;')
-encoding "$wSubject" # TODO take care of encoding
-# TODO take care of Tags, "Re:" and "Forwards"
-# TODO remove not meant separators from text before adding them
-# TODO cut filename at some point. 255 seems to be maximum, but 100 should be enough.
+unifying "$wSubject" # Unify subject
+wSubject="$unified"
+# Create a representative filename
 wtFilename="$wDate""$sep1""$wMailinglist""$sep2""$wSubject""$wCSV"
 
-# Make e-mail meta data 'tupel' permanent in .csv-format
+# Save e-mail meta data 'tupel' permanent in a .csv-formated file
+# Prepare permanent .csv-formated file 
 if [ ! -e "$wDirData"'/'"$wtFilename" ] # If the file does not yet exist...
 then # ...hence this is a new thread and...
    touch "$wDirData"'/'"$wtFilename" # ...new threads are saved in a file.
 fi
-# Create 'tupel' to be fed into file in .csv-format
+# Prepare creation of 'tupel' to be fed into file in .csv-format
 wTupelDate="$wDateString"
 wTupelUser=$(echo "$wPipedEmail" | sed '/^From: */!d; s///; q' | sed 's/.*< *//;s/ *>.*//;')
-# TODO remove not meant separators from text before adding them
+unifying "$wTupelUser"
+wTupelUser="$unified"
+# Create 'tupel' to be fed into file in .csv-format
 wTupel='"'"$wTupelDate"'"'","'"'"$wTupelUser"'"'
-# Push tupel into file
+# Push tupel into permanent in a .csv-formated file
 echo "$wTupel" >> "$wDirData"'/'"$wtFilename"
